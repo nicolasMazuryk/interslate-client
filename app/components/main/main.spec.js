a001831a5ab769b189796c1186365bf6309231b9
vladyslav.ostrenko <vladyslav.ostrenko@betlab.com>
2017-05-05 18:09:33 +0300

    add pre-commit hook

--- app/components/main/main.spec.js
+++ app/components/main/main.spec.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Main from './main'
 import {shallow} from 'enzyme'
+import store from 'core/store'
 
 describe('<Main />', () => {
   it('renders', () => {
-    const wrapper = shallow(<Main />)
+    const wrapper = shallow(<Main store={store} />)
     expect(wrapper).to.be.ok
   })
 })
