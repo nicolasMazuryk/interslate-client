d458c9b267fb157090cf772e98e96cb555c36259
vladyslav.ostrenko <vladyslav.ostrenko@betlab.com>
2017-05-12 17:04:07 +0300

    add react router

--- app/components/main/main.js
+++ app/components/main/main.js
@@ -1,6 +1,9 @@
 import React, {PureComponent} from 'react'
+import {Switch, Route} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+
+import Translations from 'components/translations/translations'
 import {loadApplication} from './actions'
 
 const mapState = (state) => {
@@ -24,7 +27,12 @@ class Main extends PureComponent {
   
   render() {
     return (
-      <div>{this.props.loaded}</div>
+      <main>
+        
+        <Switch>
+          <Route exact path="/" component={Translations} />
+        </Switch>
+      </main>
     )
   }
   
---
+++ app/components/translations/translations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Translations = () => {
+  return (
+    <div>translations</div>
+  )
+}
+
+export default Translations
